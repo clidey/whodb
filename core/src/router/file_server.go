7dc74b8fb6f2ca8e12993f0dfa3ca63cbc5d4ae0
Hemang Kandwal <hemangkandwal@gmail.com>
2024-07-05 00:46:39 +0530

    feat(frontend): fix logo png feat(core): add go:embed to include static files in the build

--- core/src/router/file_server.go
+++ core/src/router/file_server.go
@@ -1,23 +1,44 @@
 package router
 
 import (
+	"embed"
+	"io"
+	"io/fs"
 	"net/http"
-	"path/filepath"
 
+	"github.com/clidey/whodb/core/src/log"
 	"github.com/go-chi/chi/v5"
 )
 
-func fileServer(r chi.Router) {
-	fs := http.FileServer(http.Dir("./build"))
+func fileServer(r chi.Router, staticFiles embed.FS) {
+	staticFS, err := fs.Sub(staticFiles, "build")
+	if err != nil {
+		log.Logger.Fatal(err)
+	}
+
+	fs := http.FileServer(http.FS(staticFS))
+
 	r.Handle("/static/*", fs)
 	r.Handle("/images/*", fs)
-	r.Handle("/favicon.ico", fs)
 	r.Handle("/asset-manifest.json", fs)
 	r.Handle("/manifest.json", fs)
-	r.Handle("/logo192.png", fs)
-	r.Handle("/logo512.png", fs)
 	r.Handle("/robots.txt", fs)
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join("build", "index.html"))
+		file, err := staticFS.Open("index.html")
+		if err != nil {
+			http.Error(w, "index.html not found", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+
+		data, err := io.ReadAll(file)
+		if err != nil {
+			http.Error(w, "index.html read error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(data)
 	})
 }
