08479badb506398cd14afa2f72bc86d278766045
Hemang Kandwal <hemangkandwal@gmail.com>
2024-07-05 21:19:18 +0530

    fix(core): parse tiem and loc for myself defaults

--- core/src/plugins/mysql/db.go
+++ core/src/plugins/mysql/db.go
@@ -12,8 +12,8 @@ import (
 func DB(config *engine.PluginConfig) (*gorm.DB, error) {
 	port := common.GetRecordValueOrDefault(config.Credentials.Advanced, "Port", "3306")
 	charset := common.GetRecordValueOrDefault(config.Credentials.Advanced, "Charset", "utf8mb4")
-	parseTime := common.GetRecordValueOrDefault(config.Credentials.Advanced, "Parse Time", "%v")
-	loc := common.GetRecordValueOrDefault(config.Credentials.Advanced, "Loc", "%v")
+	parseTime := common.GetRecordValueOrDefault(config.Credentials.Advanced, "Parse Time", "True")
+	loc := common.GetRecordValueOrDefault(config.Credentials.Advanced, "Loc", "Local")
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=%v&parseTime=%v&loc=%v", config.Credentials.Username, config.Credentials.Password, config.Credentials.Hostname, config.Credentials.Database, port, charset, parseTime, loc)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
