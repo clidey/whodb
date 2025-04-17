7dab3db76529278327a47c55b164cc9ea63875f3
modelorona <angel.hristozov@gmail.com>
2025-04-17 12:22:27 -0700

    update anthropic models

--- core/src/llm/anthropic_client.go
+++ core/src/llm/anthropic_client.go
@@ -51,6 +51,13 @@ func prepareAnthropicRequest(c *LLMClient, prompt string, model LLMModel) (strin
 
 func getAnthropicModels(_ string) ([]string, error) {
 	models := []string{
+		"claude-3-7-sonnet-latest",
+		"claude-3-5-haiku-latest",
+		"claude-3-5-sonnet-latest",
+		"claude-3-opus-latest",
+		"claude-3-7-sonnet-20250219",
+		"claude-3-5-haiku-20241022",
+		"claude-3-5-sonnet-20241022",
 		"claude-3-5-sonnet-20240620",
 		"claude-3-opus-20240229",
 		"claude-3-sonnet-20240229",
