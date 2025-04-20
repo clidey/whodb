21243c2744e20d5ac251682ba9768c81d3c77049
modelorona <angel.hristozov@gmail.com>
2025-04-20 14:05:22 -0700

    add specific port defaults based on db time, fix postgres profile bug where input ExtraOptions was being ignored

--- core/src/plugins/gorm/db.go
+++ core/src/plugins/gorm/db.go
@@ -1,16 +1,18 @@
-// Copyright 2025 Clidey, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright 2025 Clidey, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package gorm_plugin
 
@@ -35,6 +37,18 @@ const (
 	debugKey                   = "Debug"
 	connectionTimeoutKey       = "Connection Timeout"
 )
+
+// DefaultDatabasePorts maps database systems to their standard default ports
+var defaultDatabasePorts = map[engine.DatabaseType]string{
+	engine.DatabaseType_MySQL:         "3306",
+	engine.DatabaseType_MariaDB:       "3306",
+	engine.DatabaseType_Postgres:      "5432",
+	engine.DatabaseType_Sqlite3:       "0",    // SQLite is file-based, no port
+	engine.DatabaseType_ClickHouse:    "9000", // TCP port (HTTP port is 8123)
+	engine.DatabaseType_MongoDB:       "27017",
+	engine.DatabaseType_ElasticSearch: "9200", // HTTP port (Transport port is 9300)
+	engine.DatabaseType_Redis:         "6379",
+}
 
 type ConnectionInput struct {
 	//common
@@ -62,7 +76,7 @@ type ConnectionInput struct {
 
 func (p *GormPlugin) ParseConnectionConfig(config *engine.PluginConfig) (*ConnectionInput, error) {
 	//common
-	port, err := strconv.Atoi(common.GetRecordValueOrDefault(config.Credentials.Advanced, portKey, "3306"))
+	port, err := strconv.Atoi(common.GetRecordValueOrDefault(config.Credentials.Advanced, portKey, defaultDatabasePorts[p.Type]))
 	if err != nil {
 		return nil, err
 	}
