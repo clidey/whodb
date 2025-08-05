52b728abcb3e0b3102f5f1936f836c250a8fe417
modelorona <angel.hristozov@gmail.com>
2025-08-05 22:49:14 +0100

    tentative update to the mongodb format parsing

--- core/src/plugins/mongodb/export.go
+++ core/src/plugins/mongodb/export.go
@@ -20,10 +20,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/clidey/whodb/core/src/common"
 	"github.com/clidey/whodb/core/src/engine"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -138,15 +140,66 @@ func (p *MongoDBPlugin) formatBSONValue(val any) string {
 	switch v := val.(type) {
 	case string:
 		strVal = v
-	case []any, bson.A, bson.M, map[string]any:
-		// Convert complex types to JSON
+	case bool:
+		strVal = fmt.Sprintf("%t", v)
+	case int, int8, int16, int32, int64:
+		strVal = fmt.Sprintf("%d", v)
+	case uint, uint8, uint16, uint32, uint64:
+		strVal = fmt.Sprintf("%d", v)
+	case float32, float64:
+		strVal = fmt.Sprintf("%g", v)
+	case time.Time:
+		strVal = v.Format(time.RFC3339)
+	case primitive.DateTime:
+		strVal = v.Time().Format(time.RFC3339)
+	case primitive.Timestamp:
+		strVal = fmt.Sprintf("Timestamp(%d,%d)", v.T, v.I)
+	case primitive.ObjectID:
+		strVal = v.Hex()
+	case primitive.Binary:
+		strVal = fmt.Sprintf("Binary(subtype:%d,len:%d)", v.Subtype, len(v.Data))
+	case primitive.Regex:
+		strVal = fmt.Sprintf("/%s/%s", v.Pattern, v.Options)
+	case primitive.DBPointer:
+		strVal = fmt.Sprintf("DBPointer(%s,%s)", v.DB, v.Pointer.Hex())
+	case primitive.JavaScript:
+		strVal = string(v)
+	case primitive.Symbol:
+		strVal = string(v)
+	case primitive.CodeWithScope:
+		scopeJSON, _ := json.Marshal(v.Scope)
+		strVal = fmt.Sprintf("CodeWithScope(%s, %s)", v.Code, string(scopeJSON))
+	case primitive.Decimal128:
+		strVal = v.String()
+	case primitive.MinKey:
+		strVal = "MinKey"
+	case primitive.MaxKey:
+		strVal = "MaxKey"
+	case primitive.Null:
+		strVal = "null"
+	case primitive.Undefined:
+		strVal = "undefined"
+	case []any, bson.A:
+		// Convert arrays to JSON
 		data, err := json.Marshal(v)
 		if err != nil {
 			strVal = fmt.Sprintf("%v", v)
 		} else {
 			strVal = string(data)
 		}
+	case bson.M, map[string]any, bson.D:
+		// Convert documents to JSON
+		data, err := json.Marshal(v)
+		if err != nil {
+			strVal = fmt.Sprintf("%v", v)
+		} else {
+			strVal = string(data)
+		}
+	case bson.E:
+		// Handle single BSON element
+		strVal = fmt.Sprintf("%s: %v", v.Key, v.Value)
 	default:
+		// Fallback for any other types
 		strVal = fmt.Sprintf("%v", v)
 	}
 	
