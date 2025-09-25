cf6f6cbe5b7fa45f14e2935374bbb6a4792ec180
Anguel <angel.hristozov@gmail.com>
2025-09-25 14:12:58 +0100

    add imports

--- desktop-ee/main.go
+++ desktop-ee/main.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"embed"
 
+	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 	"github.com/wailsapp/wails/v2/pkg/options/mac"
