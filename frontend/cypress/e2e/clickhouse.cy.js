eed930fc016e042f5b2b4029e0b0787afba10291
Anguel <angel.hristozov@gmail.com>
2025-11-15 14:04:22 +0000

    test fix to hide telemetry banner at the start, test fix to select pipe with index 2 instead of -1

--- frontend/cypress/e2e/clickhouse.cy.js
+++ frontend/cypress/e2e/clickhouse.cy.js
@@ -485,7 +485,7 @@ describe('Clickhouse E2E test', () => {
 
     cy.get('[role="listbox"]').should('be.visible');
 
-    cy.get('[role="option"]').eq(-1).click({force: true});
+      cy.get('[role="option"]').eq(2).click({force: true});
     cy.get('[role="listbox"]').should('not.exist');
     cy.get('[role="dialog"]').within(() => {
       cy.contains('label', 'Delimiter').parent().find('[role="combobox"]').eq(-1).invoke('text').should('include', '|');
