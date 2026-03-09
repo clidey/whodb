2a277f18ce3dedcfcf192a537e0a661327507f41
modelorona <angel.hristozov@gmail.com>
2026-03-09 21:27:35 +0000

    disable update check for ee

--- core/src/ee_plugins.go
+++ core/src/ee_plugins.go
@@ -3,12 +3,15 @@
 package src
 
 import (
+	"os"
+
 	"github.com/clidey/whodb/core/src/env"
 )
 
 func init() {
 	// Set enterprise edition flag
 	env.IsEnterpriseEdition = true
+	os.Setenv("WHODB_DISABLE_UPDATE_CHECK", "true")
 
 	// Note: The actual EE initialization is registered by the EE module itself
 	// via RegisterEEInitializer when it's imported by the main application
