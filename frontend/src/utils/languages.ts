f93a0cd330af24ecc68f717543bec0098cccb187
modelorona <angel.hristozov@gmail.com>
2026-03-17 15:22:24 +0000

    support for more languages

--- frontend/src/utils/languages.ts
+++ frontend/src/utils/languages.ts
@@ -15,13 +15,26 @@
  */
 
 export type SupportedLanguage =
-    | 'en_US'
-    | 'en_GB'
-    | 'es_ES'
-    | 'fr_FR'
+    | 'ar_AE'
     | 'de_DE'
+    | 'el_GR'
+    | 'en_GB'
+    | 'en_US'
+    | 'es_ES'
+    | 'fi_FI'
+    | 'fr_FR'
+    | 'it_IT'
+    | 'iw_IL'
+    | 'ja_JP'
+    | 'ko_KR'
+    | 'nl_NL'
+    | 'pl_PL'
     | 'pt_BR'
     | 'pt_PT'
+    | 'ro_RO'
+    | 'ru_RU'
+    | 'sk_SK'
+    | 'sv_SE'
     | 'zh_CN'
     | 'zh_HK'
     | 'zh_TW';
@@ -31,11 +44,24 @@ export const DEFAULT_LANGUAGE: SupportedLanguage = 'en_US';
 export const SUPPORTED_LANGUAGES: Record<SupportedLanguage, string> = {
     en_US: 'English (US)',
     en_GB: 'English (UK)',
-    es_ES: 'Español',
-    fr_FR: 'Français',
+    ar_AE: 'العربية',
     de_DE: 'Deutsch',
+    el_GR: 'Ελληνικά',
+    es_ES: 'Español',
+    fi_FI: 'Suomi',
+    fr_FR: 'Français',
+    it_IT: 'Italiano',
+    iw_IL: 'עברית',
+    ja_JP: '日本語',
+    ko_KR: '한국어',
+    nl_NL: 'Nederlands',
+    pl_PL: 'Polski',
     pt_BR: 'Português (Brasil)',
     pt_PT: 'Português (Portugal)',
+    ro_RO: 'Română',
+    ru_RU: 'Русский',
+    sk_SK: 'Slovenčina',
+    sv_SE: 'Svenska',
     zh_CN: '中文 (简体)',
     zh_HK: '中文 (香港)',
     zh_TW: '中文 (繁體)',
